eb58cd2d93925ce3a40bdc20bcf19c11475419db
JackyChn <ccc1020048076@gmail.com>
2024-09-09 00:05:51 +0930

    Upload: all data to supabase DB

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://cfhtzhgqirdokoimzrtg.supabase.co";
+export const supabaseUrl = "https://cfhtzhgqirdokoimzrtg.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNmaHR6aGdxaXJkb2tvaW16cnRnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU3MDAxNzgsImV4cCI6MjA0MTI3NjE3OH0._GCIDU87RJh_T09hoG16aOsi_YBG82XEXMUEFYknW9E";
 const supabase = createClient(supabaseUrl, supabaseKey);
--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,12 +1,16 @@
 import styled from "styled-components";
 import Logo from "./Logo";
 import MainNav from "./MainNav";
+import Uploader from "../data/Uploader";
 
 const StyledSideBar = styled.aside`
   background-color: var(--color-gre-0);
   padding: 3.2rem 2.4rem;
   border-right: 1px solid var(--color-grey-100);
   grid-row: 1 / -1;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
 `;
 
 function Sidebar() {
@@ -14,6 +18,7 @@ function Sidebar() {
     <StyledSideBar>
       <Logo />
       <MainNav />
+      <Uploader />
     </StyledSideBar>
   );
 }
